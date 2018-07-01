f4131c67110244e561f23143d4db364870190e71
Carsten Hartenfels <carsten.hartenfels@gmail.com>
2018-07-01 14:30:25 +0200

    Return node anim pre and post states as strings
    Instead of the magical enum values that are hard to get at.

--- lib/Model3D/Assimp/NodeAnim.pm
+++ lib/Model3D/Assimp/NodeAnim.pm
@@ -9,12 +9,32 @@ BEGIN {
     Model3D::Assimp::XS::_alias __PACKAGE__, qw(
         DESTROY       node_anim_destroy
         node_name     node_anim_node_name
-        pre_state     node_anim_pre_state
-        post_state    node_anim_post_state
         position_keys node_anim_position_keys
         rotation_keys node_anim_rotation_keys
         scaling_keys  node_anim_scaling_keys
     );
 }
+
+
+our %STATES = (
+    Model3D::Assimp::XS::aiAnimBehaviour_DEFAULT,  'default',
+    Model3D::Assimp::XS::aiAnimBehaviour_CONSTANT, 'constant',
+    Model3D::Assimp::XS::aiAnimBehaviour_LINEAR,   'linear',
+    Model3D::Assimp::XS::aiAnimBehaviour_REPEAT,   'repeat',
+);
+
+sub _to_state {
+    my ($state) = @_;
+    return $STATES{$state};
+}
+
+sub pre_state {
+    return _to_state(Model3D::Assimp::XS::node_anim_pre_state(@_));
+}
+
+sub post_state {
+    return _to_state(Model3D::Assimp::XS::node_anim_post_state(@_));
+}
+
 
 1;
